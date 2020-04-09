44cba69f0b0b00d6024b3249e6046d964bebebae
Vaisakh Murali <vaisakhmurali@gmail.com>
2020-04-09 20:26:45 +0600

    jerry: Initial Makefiles
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

---
+++ omni_wiko.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2020 Vaisakh Murali
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Device Specification
+PRODUCT_DEVICE := jerry
+PRODUCT_NAME := omni_jerry
+PRODUCT_BRAND := WIKO
+PRODUCT_MODEL := JERRY
+PRODUCT_MANUFACTURER := WIKO
